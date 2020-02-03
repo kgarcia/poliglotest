7fde7dd19990f8f1dcbf932080a541a9d5e453a7
Kevin Garcia <kevin93ps@gmail.com>
2020-02-03 12:20:18 -0300

    recurring gem working"

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :course
+  serialize :recurring, Hash
+
+  def recurring=(value)
+    if (value!="null")#RecurringSelect.is_valid_rule?(value)
+      super(RecurringSelect.dirty_hash_to_rule(value).to_hash)
+    else
+      super(nil)
+    end
+  end
 end
